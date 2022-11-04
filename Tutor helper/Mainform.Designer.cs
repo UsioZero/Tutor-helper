7d8143aac5903c6208333dd43f1eaf5db8a58a31
Oleg idi delay kyrsa4 <khotyanmisha@gmail.com>
2022-11-04 17:40:55 -0400

    try4

--- Tutor helper/Mainform.Designer.cs
+++ Tutor helper/Mainform.Designer.cs
@@ -40,16 +40,19 @@
             // studentsDataGrid
             // 
             this.studentsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.studentsDataGrid.Location = new System.Drawing.Point(130, 12);
+            this.studentsDataGrid.Location = new System.Drawing.Point(173, 15);
+            this.studentsDataGrid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.studentsDataGrid.Name = "studentsDataGrid";
-            this.studentsDataGrid.Size = new System.Drawing.Size(551, 255);
+            this.studentsDataGrid.RowHeadersWidth = 51;
+            this.studentsDataGrid.Size = new System.Drawing.Size(735, 314);
             this.studentsDataGrid.TabIndex = 0;
             // 
             // leftButtom
             // 
-            this.leftButtom.Location = new System.Drawing.Point(91, 12);
+            this.leftButtom.Location = new System.Drawing.Point(121, 15);
+            this.leftButtom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.leftButtom.Name = "leftButtom";
-            this.leftButtom.Size = new System.Drawing.Size(33, 197);
+            this.leftButtom.Size = new System.Drawing.Size(44, 313);
             this.leftButtom.TabIndex = 1;
             this.leftButtom.Text = "<";
             this.leftButtom.UseVisualStyleBackColor = true;
@@ -57,9 +60,10 @@
             // 
             // rightButton
             // 
-            this.rightButton.Location = new System.Drawing.Point(687, 12);
+            this.rightButton.Location = new System.Drawing.Point(916, 15);
+            this.rightButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rightButton.Name = "rightButton";
-            this.rightButton.Size = new System.Drawing.Size(33, 197);
+            this.rightButton.Size = new System.Drawing.Size(44, 313);
             this.rightButton.TabIndex = 2;
             this.rightButton.Text = ">";
             this.rightButton.UseVisualStyleBackColor = true;
@@ -69,17 +73,19 @@
             // 
             this.PageLabel.AutoSize = true;
             this.PageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.PageLabel.Location = new System.Drawing.Point(135, 270);
+            this.PageLabel.Location = new System.Drawing.Point(180, 332);
+            this.PageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PageLabel.Name = "PageLabel";
-            this.PageLabel.Size = new System.Drawing.Size(29, 31);
+            this.PageLabel.Size = new System.Drawing.Size(36, 39);
             this.PageLabel.TabIndex = 3;
             this.PageLabel.Text = "0";
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(713, 415);
+            this.exitButton.Location = new System.Drawing.Point(951, 511);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.Size = new System.Drawing.Size(100, 28);
             this.exitButton.TabIndex = 4;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
@@ -87,9 +93,10 @@
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(617, 273);
+            this.addButton.Location = new System.Drawing.Point(808, 337);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.Size = new System.Drawing.Size(100, 28);
             this.addButton.TabIndex = 5;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
@@ -97,15 +104,16 @@
             // 
             // Mainform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.addButton);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.PageLabel);
             this.Controls.Add(this.rightButton);
             this.Controls.Add(this.leftButtom);
             this.Controls.Add(this.studentsDataGrid);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Mainform";
             this.Text = "Tutor helper";
             ((System.ComponentModel.ISupportInitialize)(this.studentsDataGrid)).EndInit();
